985a09c3b28da0e3a52fbd9d8c76cc1abec11a95
lammiejay <lammiejay02@gmail.com>
2023-07-28 19:26:37 +0100

    started lesson in async JS

---
+++ Async Javascript/sandbox.js
@@ -0,0 +1,30 @@
+// showing what async js is
+// console.log(1);
+// console.log(2);
+
+// setTimeout(() => {
+//     console.log('callback function fired');
+// }, 2000);
+
+// console.log(3);
+// console.log(4);
+
+
+// Making HTTP requests
+const getTodos = () => {
+    const request = new XMLHttpRequest();
+
+    request.addEventListener('readystatechange', () => {
+        // console.log(request, request.readyState);
+        if (request.readyState === 4 && request.status === 200) {
+            console.log(request, request.responseText);
+        } else if(request.readyState === 4){
+            console.log("could not fetch the data");
+        }
+    });
+
+    request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
+    request.send();
+}
+
+getTodos();
