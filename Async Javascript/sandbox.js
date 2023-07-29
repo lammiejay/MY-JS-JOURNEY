c2eae21453be1fc5a5b17595cf4ac49aa2cc7ae5
lammiejay <lammiejay02@gmail.com>
2023-07-29 02:54:22 +0100

    Still on Async & Await

--- Async Javascript/sandbox.js
+++ Async Javascript/sandbox.js
@@ -11,20 +11,31 @@
 
 
 // Making HTTP requests
-const getTodos = () => {
+const getTodos = (callback) => {
     const request = new XMLHttpRequest();
 
+    // readystatechange tracks the progress of the request
     request.addEventListener('readystatechange', () => {
         // console.log(request, request.readyState);
         if (request.readyState === 4 && request.status === 200) {
-            console.log(request, request.responseText);
+            // console.log(request, request.responseText);
+            const data = JSON.parse(request.responseText)
+            callback(undefined, data);
         } else if(request.readyState === 4){
-            console.log("could not fetch the data");
+            // console.log("could not fetch the data");
+            callback('could not fetch data', undefined);
         }
     });
 
-    request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
+    request.open('GET', 'todos.json');
     request.send();
 }
 
-getTodos();
+getTodos((err, data) => {
+    console.log('callback fired');
+    if (err) {
+        console.log(err);
+    } else{
+        console.log(data);
+    }
+});
