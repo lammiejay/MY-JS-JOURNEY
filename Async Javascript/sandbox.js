2068d7bc237e95fefcd48fc755361f5151e3107b
lammiejay <lammiejay02@gmail.com>
2023-07-29 14:43:47 +0100

    Fetch API, Async & Await, Throwing and Catching Errors

--- Async Javascript/sandbox.js
+++ Async Javascript/sandbox.js
@@ -11,32 +11,39 @@
 
 
 // Making HTTP requests
-const getTodos = (resource) => {
-    const request = new XMLHttpRequest();
+// const getTodos = (resource) => {
+//     const request = new XMLHttpRequest();
 
-    return new Promise((resolve, reject) => {
+//     return new Promise((resolve, reject) => {
         // readystatechange tracks the progress of the request
-        request.addEventListener('readystatechange', () => {
-            if (request.readyState === 4 && request.status === 200) {
-                const data = JSON.parse(request.responseText)
-                resolve(data);
-            } else if(request.readyState === 4){
-                reject('error getting resource')
-            }
-        });
+    //     request.addEventListener('readystatechange', () => {
+    //         if (request.readyState === 4 && request.status === 200) {
+    //             const data = JSON.parse(request.responseText)
+    //             resolve(data);
+    //         } else if(request.readyState === 4){
+    //             reject('error getting resource')
+    //         }
+    //     });
 
-        request.open('GET', resource);
-        request.send();
+    //     request.open('GET', resource);
+    //     request.send();
 
-    })
+    // })
 
-}
+// }
 
-getTodos('todos/luigi.json').then(data => {
-    console.log('promise resolved:', data)
-}).catch(err => {
-    console.log('promise rejected');
-})
+// chaining promises
+// getTodos('todos/luigi.json').then(data => {
+//     console.log('promise 1 resolved:', data);
+//     return getTodos('todos/mario.json');
+// }).then(data => {
+//     console.log('promise 2 resolved:', data)
+//     return getTodos('todos/shaun.json');
+// }).then(data => {
+//     console.log('promise 3 resolved:', data);
+// }).catch(err => { // catches any error from either and displays where from
+//     console.log('promise rejected:', err);
+// })
 
 // callback hell
 // getTodos('todos/luigi.json', (err, data) => {
@@ -51,15 +58,15 @@ getTodos('todos/luigi.json').then(data => {
 
 // promise example
 
-const getSomething = () => {
+// const getSomething = () => {
 
-    return new Promise((resolve, reject) => {
+//     return new Promise((resolve, reject) => {
         // fetch something
         // resolve('some data'); // if its a success
         // reject('some error'); // if its an error
-    });
+//     });
 
-}
+// }
 
 // getSomething().then((data) => {
 //     console.log(data);
@@ -67,8 +74,36 @@ const getSomething = () => {
 //     console.log(err);
 // });
 
-getSomething().then(data => {
-    console.log(data);
-}).catch(err => {
-    console.log(err)
-})
+// getSomething().then(data => {
+//     console.log(data);
+// }).catch(err => {
+//     console.log(err)
+// })
+
+// = Fetch API = //
+
+// fetch('todos/luigi.json').then(response => {
+//     console.log('resolved', response);
+//     return response.json(); //returns a promise which is the data in the JSON
+// }).then(data => {
+//     console.log(data);
+// }).catch(err => {
+//     console.log('rejected', err);
+// });
+
+// async & await
+const getTodos = async () => { //an async func always returns a promise 
+
+    const response = await fetch('todos/luigi.json');
+    if (response.status !== 200) {
+        throw new Error('cannot fetch the data');
+    }
+
+    const data = await response.json();
+    return data;
+
+}
+
+getTodos()
+    .then(data => console.log('resolved:', data ))
+    .catch(err => console.log('rejected:', err.message));
